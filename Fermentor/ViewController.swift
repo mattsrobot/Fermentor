5d1929bb746cee7a648cc9e0476cbabc6acb82b1
Matthew Wilkinson <matt@somerobots.com>
2018-05-15 17:32:46 +0400

    Initial Commit

---
+++ Fermentor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Fermentor
+//
+//  Created by Matthew Wilkinson on 15/5/18.
+//  Copyright © 2018 Matthew Wilkinson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
